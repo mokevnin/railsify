d5a48c2f9d16f777bb44ed13aaeda5ce1767595a
kirill <mokevnin@gmail.com>
2013-05-30 00:23:28 +0400

    replace default generators

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,11 @@ module RailsExamples
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |g|
+      g.template_engine :haml
+      g.stylesheets false
+      g.javascripts false
+    end
   end
 end
