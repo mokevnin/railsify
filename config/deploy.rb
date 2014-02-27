7b013f4020756f5fbc8b5254979f13af7bf88a99
mokevnin <mokevnin@gmail.com>
2014-02-28 00:28:48 +0400

    restart runit after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,6 +40,8 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
+      execute :sv, "restart", "coursify"
+      execute :sv, "restart", "coursify_sidekiq"
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
     end
