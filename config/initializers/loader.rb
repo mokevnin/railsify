871d0ca707c6127f952925a62bb6c1a6c346e421
mokevnin <mokevnin@gmail.com>
2014-03-05 17:02:39 +0400

    update loader

--- config/initializers/loader.rb
+++ config/initializers/loader.rb
@@ -1,4 +1,4 @@
 #FIXME постоянные проблемы с перезгрузкой констант в деве
-# require 'custom_domain_middleware'
+require 'custom_domain_middleware'
 #
-# Course::MemberAuthorizer
+Course::MemberAuthorizer
